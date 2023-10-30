7ab29604a7cf193a0fa36c1726b8706de4799d3e
Nicholas Moen <arcanemachine@tutanota.com>
2023-10-30 16:31:34 -0600

    Add resources to router after running JSON generator

--- lib/phx_new_1_6_web/router.ex
+++ lib/phx_new_1_6_web/router.ex
@@ -18,6 +18,7 @@ defmodule PhxNew16Web.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/persons", PersonController, except: [:new, :edit]
   end
 
   # Other scopes may use custom stacks.
